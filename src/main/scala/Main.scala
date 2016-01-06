ccfd0413ddf358f82d8fa1a547155d287121859e
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2016-01-05 21:22:01 -0800

    Collect Scala files without either SBT or Ant

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,28 @@
 package com.pactdisc.scaladoc
+
+import java.nio.file.{Path, Paths, Files}
+import java.util.function.Predicate
+import java.util.stream.Collectors
+import collection.JavaConverters._
+
 import tools.nsc
 
 object Main {
-  def main(args: Array[String]) = {
-    nsc.ScalaDoc.main(Array("-usejavacp", "-doc-generator", "com.pactdisc.scaladoc.Doclet", "-d", "build", "src/main/scala/Main.scala"))
+  def listFiles(root: String): Iterable[Path] = {
+    val predicate = new Predicate[Path] {
+      override def test(path: Path) = {
+        path.toString.endsWith(".scala")
+      }
+    }
+    Files.walk(Paths.get(root)).filter(predicate).collect(Collectors.toList()).asScala
+  }
+
+  def main(args: Array[String]) {
+    val files = args.flatMap(listFiles).map(_.toString)
+    nsc.ScalaDoc.main(Array(
+      "-usejavacp",
+      "-doc-generator", "com.pactdisc.scaladoc.Doclet",
+      "-d", "build"
+    ) ++ files)
   }
 }
