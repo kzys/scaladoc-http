30fc857730a646b651b4a857f5a01872892b8243
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2016-01-09 20:53:47 -0800

    Add a place for an upcoming sidebar

--- src/main/scala/com/pactdisc/scaladoc/EntityPage.scala
+++ src/main/scala/com/pactdisc/scaladoc/EntityPage.scala
@@ -1,11 +1,27 @@
 package com.pactdisc.scaladoc
+
+import scala.tools.nsc.doc.html.HtmlPage
 import tools.nsc.doc.Universe
 import tools.nsc.doc.html.page.Template
 import tools.nsc.doc.html.page.diagram.DiagramGenerator
 import tools.nsc.doc.model.DocTemplateEntity
-import scalatags.Text.all._
 
-class EntityPage(universe: Universe, diagramGenerator: DiagramGenerator, entity: DocTemplateEntity) extends Template(universe, diagramGenerator, entity) {
+class EntityPage(universe: Universe, diagramGenerator: DiagramGenerator, entity: DocTemplateEntity) extends HtmlPage {
+  // Since "body" is defined as val in Template, inheriting from the class doesn't work
+  val original = new Template(universe, diagramGenerator, entity)
+
   override val headers =
-    Tag.group(link(rel := "stylesheet", href := relativeLinkTo(List("template.css", "lib"))))
+    <xml:group>
+      <link rel="stylesheet" href="/assets/template.css" />
+    </xml:group>
+
+  override def body =
+    <xml:group>
+      <div id="sidebar"></div>
+      <div id="body">{original.body}</div>
+      <script type="text/javascript" src="/assets/template.js"></script>
+    </xml:group>
+
+  override protected def title = original.title
+  override def path = original.path
 }
