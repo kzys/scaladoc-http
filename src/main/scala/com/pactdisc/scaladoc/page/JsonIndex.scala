910f3a9be7b2ac38f27cf8ad53151d4ac29e56c4
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2016-01-17 15:18:50 -0800

    Add classes, objects and methods

--- src/main/scala/com/pactdisc/scaladoc/page/JsonIndex.scala
+++ src/main/scala/com/pactdisc/scaladoc/page/JsonIndex.scala
@@ -1,23 +1,56 @@
 package com.pactdisc.scaladoc.page
 
+import scala.collection.mutable
 import scala.tools.nsc.doc
 import scala.tools.nsc.doc.html.{Page, HtmlFactory}
+import scala.tools.nsc.doc.model.{DocTemplateEntity, MemberEntity}
 import scala.util.parsing.json.{JSONArray, JSONObject}
 
 /**
   * Created by kazuyoshi on 1/5/16.
   */
 class JsonIndex(universe: doc.Universe, val index: doc.Index) extends Page {
-  private def listPackage(root: doc.model.Package): Seq[JSONObject] = {
-    val obj = Map("name" -> root.name, "path" -> root.qualifiedName)
-    List(JSONObject(obj))
+  private def collectSymbolsFromTemplate(symbols: mutable.Map[String, List[String]], template: DocTemplateEntity): Unit = {
+    for (method <- template.methods) {
+      val xs = symbols.getOrElse(method.name, List.empty)
+      val qualifiedName = s"${template.qualifiedName} ${method.signature}"
+      symbols.update(method.name, qualifiedName :: xs)
+    }
+  }
+
+  private def collectSymbols(symbols: mutable.Map[String, List[String]], root: doc.model.Package): Unit = {
+    for (template <- root.templates) {
+      template match {
+        case entity: DocTemplateEntity => collectSymbolsFromTemplate(symbols, entity)
+        case _ => ;
+      }
+
+      val xs = symbols.getOrElse(template.name, List.empty)
+      val qualifiedName = if (template.isObject) {
+        template.qualifiedName + " object"
+      } else {
+        template.qualifiedName
+      }
+
+      symbols.update(template.name, qualifiedName :: xs)
+    }
+
+    for (pkg <- root.packages) {
+      val xs = symbols.getOrElse(pkg.name, List.empty)
+      symbols.update(pkg.name, pkg.qualifiedName :: xs)
+      collectSymbols(symbols, pkg)
+    }
   }
 
   override def writeFor(site: HtmlFactory) {
-    val packages = universe.rootPackage.packages.flatMap(listPackage)
-    writeFile(site) {
-      _.write(JSONArray(packages).toString())
-    }
+    val symbols = mutable.Map.empty[String, List[String]]
+    collectSymbols(symbols, universe.rootPackage)
+
+    val newMap = symbols.map({
+      case (key, value) => key -> JSONArray(value)
+    }).toMap
+
+    writeFile(site) { _.write(JSONObject(newMap).toString()) }
   }
 
   override def path = List("index.json")
