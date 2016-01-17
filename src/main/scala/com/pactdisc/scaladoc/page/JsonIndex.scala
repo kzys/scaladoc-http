eb06d1d9bd913df4362106f2f7617434c638c730
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2016-01-17 12:04:29 -0800

    Back to 2.12.0-M3 again

--- src/main/scala/com/pactdisc/scaladoc/page/JsonIndex.scala
+++ src/main/scala/com/pactdisc/scaladoc/page/JsonIndex.scala
@@ -1,26 +1,22 @@
 package com.pactdisc.scaladoc.page
 
-import scala.collection.mutable
 import scala.tools.nsc.doc
 import scala.tools.nsc.doc.html.{Page, HtmlFactory}
-import org.json4s._
-import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
+import scala.util.parsing.json.{JSONArray, JSONObject}
 
 /**
   * Created by kazuyoshi on 1/5/16.
   */
 class JsonIndex(universe: doc.Universe, val index: doc.Index) extends Page {
-  private def listPackage(root: doc.model.Package): Seq[JObject] = {
-    val obj = ("name" -> root.name) ~ ("path" -> root.qualifiedName)
-    obj +: root.packages.flatMap((pkg) => {
-      listPackage(pkg)
-    })
+  private def listPackage(root: doc.model.Package): Seq[JSONObject] = {
+    val obj = Map("name" -> root.name, "path" -> root.qualifiedName)
+    List(JSONObject(obj))
   }
+
   override def writeFor(site: HtmlFactory) {
     val packages = universe.rootPackage.packages.flatMap(listPackage)
     writeFile(site) {
-      _.write(compact(render(packages)))
+      _.write(JSONArray(packages).toString())
     }
   }
 
